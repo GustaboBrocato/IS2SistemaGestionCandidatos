d77d66907b25b03792f8b92165290f15cd4a6515
Gustavo <tavo198704@gmail.com>
2024-12-07 01:09:56 -0600

    Arreglo Imagen Usuario

---
+++ javascript/upload_imageUser.js
@@ -0,0 +1,62 @@
+import BASE_URL from '../javascript/config.js';
+const changePictureButton = document.getElementById('changePictureButton');
+const uploadPictureInput = document.getElementById('uploadPictureInput');
+const recruiterProfilePicture = document.getElementById('recruiterProfilePicture');
+const candidateProfilePicture = document.getElementById('candidateProfilePicture');
+
+// Abrir el selector de imágenes
+changePictureButton.addEventListener('click', () => {
+    uploadPictureInput.click();
+});
+
+// Manejo de selección de imagen
+uploadPictureInput.addEventListener('change', async (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        const formData = new FormData();
+        formData.append('image', file);
+
+        const token = localStorage.getItem('token');
+
+        try {
+            const endpoint = "/api/images/upload";
+            const url = `${BASE_URL}${endpoint}`;
+            const response = await fetch(url, {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            const result = await response.json();
+            if (response.ok) {
+                showToastMessage("¡Foto de perfil actualizada exitosamente!");
+                setTimeout(() => {
+                    window.location.reload();
+                }, 1000);
+            } else {
+                showToastMessage("No se pudo cargar la imagen:" + result.message);
+            }
+        } catch (error) {
+            console.error("Error al cargar la imagen:", error);
+            showToastMessage("Error al cargar la imagen. Por favor inténtalo de nuevo.");
+        }
+    }
+});
+
+// Función para mostrar mensaje
+function showToastMessage(message) {
+    const toastMessage = document.getElementById('toastMessage');
+    toastMessage.textContent = message;
+    toastMessage.style.display = 'block';
+
+    // Hide the toast message after 3 seconds
+    setTimeout(() => {
+        toastMessage.style.display = 'none';
+    }, 3000);
+}
+
+
+
+
